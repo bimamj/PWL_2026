b8e5bfe1528a4c33f2b3c46483b32c923f4a2520
M Bima J <bimamj7@gmail.com>
2026-02-24 17:33:56 +0700

    praktikum 3

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,4 +10,14 @@ class WelcomeController extends Controller
     	    return 'Hello World';
 	}
 
+	// public function greeting() {
+	// 	    return view('blog.hello', ['name' => 'Andi']);
+	// }
+
+	public function greeting() {
+		return view('blog.hello')
+			->with('name', 'Andi')
+			->with('occupation', 'Astronaut');
+	}
+
 }
--- routes/web.php
+++ routes/web.php
@@ -79,4 +79,9 @@ Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
 
+// Route::get('/greeting', function () {
+// 	return view('blog.hello', ['name' => 'Andi']);
+// });
+
+Route::get('/greeting', [WelcomeController::class,'greeting']);
 
