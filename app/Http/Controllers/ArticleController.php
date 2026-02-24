4545bc57404cb38c7bb98557c279df60a5c03cf8
M Bima J <bimamj7@gmail.com>
2026-02-24 17:17:22 +0700

    praktikum 2

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function article($id) {
+    	    return 'Halaman Artikel dengan ID: '.$id;
+	}
+
+    // public function article($id) {
+    // 	    return 'Halaman Artikel dengan ID: '.$id;
+	// }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController; 
+use App\Http\Controllers\PhotoController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,9 +31,14 @@ Route::match(['get', 'post'], '/specialUrl', function () {
 Route::any('/specialMahasiswa', function ($id) {
 });
 
-Route::get('/hello', function () {
-   return 'Hello World';
-});
+// Route::get('/hello', function () {
+//    return 'Hello World';
+// });
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+Route::get('/about', [AboutController::class,'about']);
+Route::get('/article/{id}', [ArticleController::class,'article']);
+
 
 Route::get('/world', function () {
    return 'World';
@@ -39,9 +48,9 @@ Route::get('/', function () {
    return 'Selamat Datang';
 });
 
-Route::get('/about', function () {
-   return '244107020123';
-});
+// Route::get('/about', function () {
+//    return '244107020123';
+// });
 
 Route::get('/user/{name}', function ($name) {
 return 'Nama saya '.$name;
@@ -51,10 +60,23 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
 });
 
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman Artikel dengan ID: '.$id;
-});
+// Route::get('/articles/{id}', function ($id) {
+//     return 'Halaman Artikel dengan ID: '.$id;
+// });
 
 Route::get('/user/{name?}', function ($name='John') {
 return 'Nama saya '.$name;
 });
+
+Route::resource('photos', PhotoController::class);
+
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
+
+
